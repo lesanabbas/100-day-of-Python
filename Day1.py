75aedd0bba31b1ce7d2b3299d81bc3f8d0642ca5
lesanabbas <63160640+lesanabbas@users.noreply.github.com>
2021-07-23 13:22:55 +0530

    commit

---
+++ Day1.py
@@ -0,0 +1,8 @@
+def bandNameGenerator():
+    city = input("Which city do you live in: ")
+    pet = input("What's your pet name: ")
+    print(f"\nYour band name should be {city} {pet}")
+
+
+if __name__ == '__main__':
+    bandNameGenerator()
